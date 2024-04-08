a4d48fd754c0a5cdbf81b1f5bfe6771a45606856
SreeHarsha-Kamisetty <sreeharsha.kamisetty99@gmail.com>
2024-04-08 22:06:39 +0530

    user routes created

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,18 @@
 const express = require("express")
-const cors = require("cors")
+const cors = require("cors");
+const UserRouter = require("./routes/users.routes");
+
 
 
 const app = express();
 app.use(cors())
+app.use(express.json())
 
-
+app.use("/users",UserRouter)
 app.get("/",(req,res)=>{
     res.send("Home")
 })
+
 
 
 app.listen(8080,()=>{
